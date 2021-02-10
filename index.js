1ee71a7d4dcd7b0912879f56d0714528d8873326
liudmila arzamasova <liudmila_arzamasava@epam.com>
2021-02-10 13:34:57 +0300

    setup webpack with different environments

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./src/components/App.js";
+
+// comment for remove-comments-loader
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import '../styles/index.css';
+
+const App = () => {
+
+  // comment for remove-comments-loader
+  return (
+    <div>
+      <h2>Hello Webpack!</h2>
+    </div>
+  )
+}
+
+export default App;
