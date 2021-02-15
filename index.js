75ae90c23497eca91956c7036f38538f1cd51782
Liudmila Arzamasova <liudmila.arzamasova@gmail.com>
2021-02-15 17:33:42 +0300

    remove extra comments

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./src/components/App.js";
-
-// comment for remove-comments-loader
+import App from "./src/components/App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
