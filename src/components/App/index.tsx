9bb034f54a6f903ce00a65bff41e1cb06fcfc83e
Liudmila Arzamasova <Liudmila.Arzamasova@gmail.com>
2021-02-26 14:05:22 +0300

    create Backdrop component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from 'react';
 
-import { App as AppStyled } from './style';
+import { App as AppStyled, Backdrop } from './style';
 import { GlobalStyle } from '../../style';
 
 import Header from 'components/Header';
 import Catalog from 'components/Catalog';
 import Footer from 'components/Footer';
+import AddMovie from 'components/AddMovie';
 
 const App: FC = () => (
   <AppStyled>
     <GlobalStyle />
+    <Backdrop className="backdrop--active"></Backdrop>
+    <AddMovie />
     <Header />
     <Catalog />
     <Footer />
--- src/components/App/style.ts
+++ src/components/App/style.ts
@@ -3,3 +3,19 @@ import styled from 'styled-components';
 export const App = styled.div`
   margin: 0 auto;
 `;
+
+export const Backdrop = styled.div`
+  &.backdrop {
+    &--active {
+      position: absolute;
+      width: 100%;
+      height: 100vw;
+      max-height: 100vh;
+      background-color: rgba(12, 13, 14, 0.7);
+      backdrop-filter: blur(50px);
+
+      opacity: 0.7;
+      z-index: 5;
+    }
+  }
+`;
