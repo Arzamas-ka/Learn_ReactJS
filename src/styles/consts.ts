c71d3f4aadc22b3a64fe8f5588cf0708fb7dbd20
Liudmila Arzamasova <Liudmila.Arzamasova@gmail.com>
2021-03-09 01:08:56 +0300

    create breakpoints for @media

---
+++ src/styles/consts.ts
@@ -0,0 +1,29 @@
+import { css } from 'styled-components';
+
+const size = {
+  xs: `370px`,
+  s: `380px`,
+  xm: `430px`,
+  xxm: `450px`,
+  m: `490px`,
+  xl: `510px`,
+  l: `600px`,
+  xlg: `970px`,
+  lg: `880px`,
+};
+
+export const breakpoints = {
+  xs: `(max-width: ${size.xs})`,
+  s: `(max-width: ${size.s})`,
+  xm: `(max-width: ${size.xm})`,
+  xxm: `(max-width: ${size.xxm})`,
+  m: `(max-width: ${size.m})`,
+  xl: `(max-width: ${size.xl})`,
+  l: `(max-width: ${size.l})`,
+  xlg: `(max-width: ${size.xlg})`,
+  lg: `(max-width: ${size.lg})`,
+};
+
+// export colors = {
+
+// }
