4245b489fb30c4c096ac06bff43550bdeb32ef0b
Liudmila Arzamasova <Liudmila.Arzamasova@gmail.com>
2021-03-17 18:37:16 +0300

    fixed Footer into bottom

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -6,5 +6,10 @@ export const StyledFooter = styled.div`
   justify-content: center;
   align-items: center;
 
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+
   background-color: ${colors.grey_light}; ;
 `;
