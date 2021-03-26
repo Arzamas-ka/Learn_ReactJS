8ced9166965965c36557cf99072ad5f0dce44ceb
Liudmila Arzamasova <Liudmila.Arzamasova@gmail.com>
2021-03-26 12:47:44 +0300

    create custom Select with logic

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,27 +1,62 @@
-import React, { FC } from 'react';
-import shortid from 'shortid';
+import React, { FC, useState } from 'react';
 
 import { SelectProps } from './models';
 import {
   StyledSelectWrapper,
   StyledSelectTitle,
   StyledSelectMain,
+  StyledSelectOver,
+  StyledSelectLabel,
+  StyledSelectInput,
 } from './style';
 
 import { SELECT_OPTIONS } from '@constants';
 
-const Select: FC<SelectProps> = ({ onChange, name }) => {
-  const options = SELECT_OPTIONS.map((option) => (
-    <option key={shortid.generate()}>{option}</option>
-  ));
+const Select: FC<SelectProps> = () => {
+  const [isOpen, setOpen] = useState(false);
+  const [selected, setSelected] = useState([]);
+
+  const handleOnSelected = (selectedOption) => {
+    if (selected.includes(selectedOption)) {
+      const newSelected = selected.filter(
+        (option) => option !== selectedOption,
+      );
+      setSelected(newSelected);
+    } else {
+      const newSelected = [...selected, selectedOption];
+      setSelected(newSelected);
+    }
+  };
+
+  const options = SELECT_OPTIONS.map((option) => {
+    return (
+      <StyledSelectLabel key={option}>
+        <StyledSelectInput
+          type="checkbox"
+          id={option}
+          onChange={() => {
+            handleOnSelected(option);
+          }}
+        />
+        {option}
+      </StyledSelectLabel>
+    );
+  });
+
+  const value = selected.join(' ');
+
+  console.log('selected: ', selected);
 
   return (
     <StyledSelectWrapper>
       <StyledSelectTitle>Genre</StyledSelectTitle>
-      <StyledSelectMain name={name} onChange={onChange}>
-        <option hidden>Select Genre</option>
-        {options}
-      </StyledSelectMain>
+      <StyledSelectMain
+        defaultValue={value}
+        type="text"
+        onClick={() => setOpen((isOpen) => !isOpen)}
+        placeholder="Select Genre"
+      ></StyledSelectMain>
+      {isOpen && <StyledSelectOver>{options}</StyledSelectOver>}
     </StyledSelectWrapper>
   );
 };
